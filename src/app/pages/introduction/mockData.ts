2968e7abcf7c3911c894429ae557343c598d563e
huynguyen <huynguyen1998169@gmail.com>
2020-12-30 06:38:34 +0700

    update

--- src/app/pages/introduction/mockData.ts
+++ src/app/pages/introduction/mockData.ts
@@ -1,22 +1,22 @@
 export const data = [
     {
-        img: 'https://images.unsplash.com/photo-1525507119028-ed4c629a60a3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
+        img: 'https://www.apparelresources.com/wp-content/uploads/2014/08/Lectra_Kaledo-V4R1-copy-copy.jpg',
         title: 'Giải pháp mua đồ mới lạ .',
         des: 'Quên đi những khoản chi trả kha khá và phải đi nhiều nơi lẻ tẻ để có được những món đồ cơ bản nhất như áo thun, quần short, quần sịp, tất (vớ), chúng tôi mong muốn thay đổi mọi thứ. Chỉ bằng vài cú click chuột và một tủ đồ đầy đủ sẽ đến gõ cửa nhà bạn ngay sau đó.'
     },
     {
-        img: 'https://images.unsplash.com/photo-1525507119028-ed4c629a60a3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
-        title: 'Giải pháp mua đồ mới lạ .',
-        des: 'Quên đi những khoản chi trả kha khá và phải đi nhiều nơi lẻ tẻ để có được những món đồ cơ bản nhất như áo thun, quần short, quần sịp, tất (vớ), chúng tôi mong muốn thay đổi mọi thứ. Chỉ bằng vài cú click chuột và một tủ đồ đầy đủ sẽ đến gõ cửa nhà bạn ngay sau đó.'
+        img: 'https://image.freepik.com/free-vector/customer-with-shopping-cart-buying-digital-service-online-digital-service-marketplace-ready-digital-solution-online-marketplace-framework-concept_335657-894.jpg',
+        title: 'Trải nghiệm mua sắm thông minh.',
+        des: 'Đem lại sự thoải mái nhất trong mua sắm. Bạn có thể tự do xem bất kỳ món hàng nào, theo dõi những món đồ muốn mua, mua hàng trong tích tắc và thậm chí là có thể đổi trả 45 ngày miễn phí vì bất kỳ lý do gì.'
     },
     {
-        img: 'https://images.unsplash.com/photo-1525507119028-ed4c629a60a3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
-        title: 'Giải pháp mua đồ mới lạ .',
-        des: 'Quên đi những khoản chi trả kha khá và phải đi nhiều nơi lẻ tẻ để có được những món đồ cơ bản nhất như áo thun, quần short, quần sịp, tất (vớ), chúng tôi mong muốn thay đổi mọi thứ. Chỉ bằng vài cú click chuột và một tủ đồ đầy đủ sẽ đến gõ cửa nhà bạn ngay sau đó.'
+        img: 'https://cdn5.vectorstock.com/i/1000x1000/64/89/low-price-stamp-vector-19036489.jpg',
+        title: 'Giá cả hợp lý',
+        des: 'Tất cả những gì bạn thấy trên web là tất cả những gì bạn phải trả, cam kết không có chi phí phát sinh trong quá trình mua và đổi trả hàng.'
     },
     {
-        img: 'https://images.unsplash.com/photo-1525507119028-ed4c629a60a3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
-        title: 'Giải pháp mua đồ mới lạ .',
-        des: 'Quên đi những khoản chi trả kha khá và phải đi nhiều nơi lẻ tẻ để có được những món đồ cơ bản nhất như áo thun, quần short, quần sịp, tất (vớ), chúng tôi mong muốn thay đổi mọi thứ. Chỉ bằng vài cú click chuột và một tủ đồ đầy đủ sẽ đến gõ cửa nhà bạn ngay sau đó.'
+        img: 'https://eprtech.com/wp-content/uploads/2018/10/Customer-Care.png',
+        title: 'Sẵn sàng hỗ trợ việc mua sắm',
+        des: 'Dịch vụ hỗ trợ và tư vấn khách hàng 24/7, hướng đến giao hàng trong 4h tại các thành phố lớn, 45 ngày đổi trả miễn phí với bất kì lý do gì, kể cả là đồ lót, chúng tôi không ngại đổi/trả và luôn chấp nhận để đem lại trải nghiệm tốt nhất đến khách hàng.'
     },
 ]
